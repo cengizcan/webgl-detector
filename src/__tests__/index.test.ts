7dd6fbfde437785808c5102a7386c04621144495
Cengiz Can <cengiz@linux.com>
2019-08-25 01:12:00 +0300

    1.1.0

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -6,38 +6,37 @@ import { isWebGLSupported } from '../index';
 // https://github.com/TypeStrong/ts-node/issues/55
 
 describe('WebGL detector tests', () => {
-  
   it('should return false if window.WebGLRenderingContext is not extists.', () => {
     Object.defineProperty(window, 'WebGLRenderingContext', {
       writable: true,
-      value: undefined
+      value: undefined,
     });
     Object.defineProperty(document, 'createElement', {
       writable: true,
-      value: () => ({ getContext: (ctx:string) => ctx === 'experimental-webgl' })
+      value: () => ({ getContext: (ctx: string) => ctx === 'experimental-webgl' }),
     });
     expect(isWebGLSupported()).toBe(false);
   });
   it('should return true if webgl context exists.', () => {
     Object.defineProperty(window, 'WebGLRenderingContext', {
       writable: true,
-      value: () => true
+      value: () => true,
     });
     Object.defineProperty(document, 'createElement', {
       writable: true,
-      value: () => ({ getContext: (ctx:string) => ctx === 'webgl' })
+      value: () => ({ getContext: (ctx: string) => ctx === 'webgl' }),
     });
     expect(isWebGLSupported()).toBe(true);
   });
   it('should return true if experimental-webgl context exists.', () => {
     Object.defineProperty(window, 'WebGLRenderingContext', {
       writable: true,
-      value: () => true
+      value: () => true,
     });
 
     Object.defineProperty(document, 'createElement', {
       writable: true,
-      value: () => ({ getContext: (ctx:string) => ctx === 'experimental-webgl' })
+      value: () => ({ getContext: (ctx: string) => ctx === 'experimental-webgl' }),
     });
     expect(isWebGLSupported()).toBe(true);
   });
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-export function isWebGLSupported():boolean {
+export function isWebGLSupported(): boolean {
   if (typeof WebGLRenderingContext === 'function') {
-    const canvas:HTMLCanvasElement = document.createElement('canvas');
+    const canvas: HTMLCanvasElement = document.createElement('canvas');
     return canvas.getContext('webgl') != null || canvas.getContext('experimental-webgl') != null;
   }
   return false;
